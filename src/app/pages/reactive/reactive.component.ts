c78bfe95dc131470a2ebc2751a5f1693213c50bf
Diego Pagini <diegopagini777@gmail.com>
2021-01-02 15:10:40 -0300

    html listo

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
-  styleUrls: ['./reactive.component.css']
+  styleUrls: ['./reactive.component.css'],
 })
 export class ReactiveComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  forma: FormGroup;
+  constructor(private form: FormBuilder) {
+    this.crearFormulario();
   }
 
+  ngOnInit(): void {}
+
+  crearFormulario() {
+    this.forma = this.form.group({
+      nombre: [''],
+      apellido: [''],
+      correo: [''],
+    });
+  }
+
+  guardar() {
+    console.log(this.forma);
+  }
 }
